e3bb4732af956249c363bde62383cf2c08be8c29
sks1729 <surav_02@live.com>
2020-10-02 13:46:24 +0545

    first commit

---
+++ Programs/anagrams.py
@@ -0,0 +1,47 @@
+"""
+    Two or more words are said to be Anagram to each other if they have
+    the same alphabets, after non-alphabets are neglected
+
+    STEPS INVOLVED:
+1.  we replace all the non-alphabets with empty-string ""
+2.  then we sort the string and compare them using sorted()
+    if the sorted strings are equal, they are anagram else
+    they are not anagram
+"""
+
+
+# returns the strings from the list lst which are anagram to string
+def anagrams(string, lst):
+    anagrams = []
+
+    new_str = ""
+
+    # ASCII code of lower-case alphabets (97 to 122)
+    # convert all non lower-case alphabets to ""
+    for char in string.lower():
+        if ord(char) in range(97, 123):
+            new_str += char
+        else:
+            new_str += ""
+
+    sort_string = sorted(new_str)
+
+    # ASCII code of lower-case alphabets (97 to 122)
+    # convert all non lower-case alphabets to ""
+    for i in range(len(lst)):
+        new_str = ""
+        for char in lst[i].lower():
+            if ord(char) in range(97, 123):
+                new_str += char
+            else:
+                new_str += ""
+        if sorted(new_str) == sort_string:
+            anagrams.append(lst[i])
+
+    if anagrams:
+        return anagrams
+    else:
+        return ("No match found")
+
+
+print(anagrams("McDonald's restaurants", ["Church of Scientology", "Uncle Sam's standard rot"]))
